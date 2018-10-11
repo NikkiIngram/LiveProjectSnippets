d9ca1342f7b4bdc59abb3344b5c105c9963e8f41
Nikki Ingram <niking278@gmail.com>
2018-10-11 15:04:08 -0500

    Add files via upload

---
+++ JPStudentRundown.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+//updated JPStudentRundown parameters
+
+public class JPStudentRundownChanges
+{
+    //public JPStudentRundown(string name, string email, string loc, int daycount, string linkedin, string port, int totalApplications, int totalApplicationsThisWeek, int jpid, bool jpGraduated, int calculateLastContactDate, int checkListStatus)
+        public JPStudentRundown(JPStudent student, int totalApplications, int totalApplicationsThisWeek, int calculateLastContactDate, int checkListStatus)
+    {
+        StudentName = name;
+        StudentEmail = email;
+        StudentLocation = loc;
+        StudentDayCount = daycount;
+        StudentLinkedIn = linkedin;
+        StudentPortfolio = port;
+        StudentName = student.JPName;
+        StudentEmail = student.JPEmail;
+        StudentLocation = student.JPStudentLocation.ToString();
+        StudentDayCount = student.DaysSinceStart;
+        StudentLinkedIn = student.JPLinkedIn;
+        StudentPortfolio = student.JPPortfolio;
+        TotalApplications = totalApplications;
+        TotalApplicationsThisWeek = totalApplicationsThisWeek;
+        JPStudentId = jpid;
+        JPGraduated = jpGraduated;
+        JPStudentId = student.JPStudentId;
+        JPGraduated = student.JPGraduated;
+        CalculateLastContactDate = calculateLastContactDate;
+        CheckListStatus = checkListStatus;
+    }
+    //public JPStudentRundown(string name, string email, string loc, int daycount, string linkedin, string port, int jpid)
+     public JPStudentRundown(JPStudent student)
+    {
+        StudentName = name;
+        StudentEmail = email;
+        StudentLocation = loc;
+        StudentDayCount = daycount;
+        StudentLinkedIn = linkedin;
+        StudentPortfolio = port;
+        JPStudentId = jpid;
+        StudentName = student.JPName;
+        StudentEmail = student.JPEmail;
+        StudentLocation = student.JPStudentLocation.ToString();
+        StudentDayCount = student.DaysSinceStart;
+        StudentLinkedIn = student.JPLinkedIn;
+        StudentPortfolio = student.JPPortfolio;
+        JPStudentId = student.JPStudentId;
+    }
+}
---
+++ SnapshotViewModelController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+//calculated avg job placement days by location on SnapshotViewModelController
+
+public class SnapshotViewModelControllerChanges
+{
+    public SnapshotViewModelControllerChanges()
+    {
+        int totalDaysInJP = 0, portlandDaysInJP = 0, denverDaysInJP = 0, seattleDaysInJP = 0, remoteDaysInJP = 0;
+        int totalStudents = 0, portlandStudents = 0, denverStudents = 0, seattleStudents = 0, remoteStudents = 0;
+        int avgDaysInJP = 0, portlandAvgDaysInJP = 0, denverAvgDaysInJP = 0, seattleAvgDaysInJP = 0, remoteAvgDaysInJP = 0;
+
+        foreach (var student in db.JPStudents)
+        {
+            int id = student.JPStudentId;
+
+            if (student.JPHired == true)
+            {
+                if ((student.JPStudentLocation == JPStudentLocation.PortlandLocal) || (student.JPStudentLocation == JPStudentLocation.PortlandRemote))
+                {
+                    portlandDaysInJP += (int)(DateTime.Now - student.JPStartDate).TotalDays;
+                    portlandStudents++;
+                }
+                if ((student.JPStudentLocation == JPStudentLocation.DenverLocal) || (student.JPStudentLocation == JPStudentLocation.DenverRemote))
+                {
+                    denverDaysInJP += (int)(DateTime.Now - student.JPStartDate).TotalDays;
+                    denverStudents++;
+                }
+                if ((student.JPStudentLocation == JPStudentLocation.SeattleLocal) || (student.JPStudentLocation == JPStudentLocation.SeattleRemote))
+                {
+                    seattleDaysInJP += (int)(DateTime.Now - student.JPStartDate).TotalDays;
+                    seattleStudents++;
+                }
+                if ((student.JPStudentLocation == JPStudentLocation.Remote))
+                {
+                    remoteDaysInJP += (int)(DateTime.Now - student.JPStartDate).TotalDays;
+                    remoteStudents++;
+                }
+                totalDaysInJP += (int)(DateTime.Now - student.JPStartDate).TotalDays;
+                totalStudents++;
+            }
+        }
+
+        if (portlandStudents > 0) { portlandAvgDaysInJP = (portlandDaysInJP / portlandStudents); }
+        if (denverStudents > 0) { denverAvgDaysInJP = (denverDaysInJP / denverStudents); }
+        if (seattleStudents > 0) { seattleAvgDaysInJP = (seattleDaysInJP / seattleStudents); }
+        if (remoteStudents > 0) { remoteAvgDaysInJP = (remoteDaysInJP / remoteStudents); }
+        avgDaysInJP = (totalDaysInJP / totalStudents);
+
+        var snapshotStats = new SnapshotViewModel(newJpStudentsList, weeklyHiresList, totalWeeklyApps, totalWeeklyHires, jpStudentCount, unhiredGradCount,
+            newJpStudentsList_Portland, weeklyHiresList_Portland, totalWeeklyApps_Portland, totalWeeklyHires_Portland, jpStudentCount_Portland, unhiredGradCount_Portland,
+            newJpStudentsList_Denver, weeklyHiresList_Denver, totalWeeklyApps_Denver, totalWeeklyHires_Denver, jpStudentCount_Denver, unhiredGradCount_Denver,
+            newJpStudentsList_Seattle, weeklyHiresList_Seattle, totalWeeklyApps_Seattle, totalWeeklyHires_Seattle, jpStudentCount_Seattle, unhiredGradCount_Seattle,
+            newJpStudentsList_Remote, weeklyHiresList_Remote, totalWeeklyApps_Remote, totalWeeklyHires_Remote, jpStudentCount_Remote, unhiredGradCount_Remote,
+            avgDaysInJP, portlandAvgDaysInJP, denverAvgDaysInJP, seattleAvgDaysInJP, remoteAvgDaysInJP, totalDaysInJP, portlandDaysInJP, denverDaysInJP, seattleDaysInJP, remoteDaysInJP
+            );
+
+    }
+}
